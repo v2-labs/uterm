d75668b96ca45cc840f0da8ca7f9cef4fe23e649
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2016-10-27 13:46:23 -0200

    Initial Commit

---
+++ uTerm/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  uTerm
+//
+//  Created by Juvenal A. Silva Jr. on 27/10/16.
+//  Copyright © 2016 v2-lab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
