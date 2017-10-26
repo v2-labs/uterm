323e064e9363b4b59cc9089e4dc082a4f721de17
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2017-10-26 08:27:13 -0200

    Update to Swift 4
    This patch provides the initial update to Swift 4 language.

--- uTerm/StatusMenuController.swift
+++ uTerm/StatusMenuController.swift
@@ -12,11 +12,11 @@ class StatusMenuController: NSObject {
 
     @IBOutlet weak var statusMenu: NSMenu!
 
-    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
 
     override func awakeFromNib() {
-        let icon = NSImage(named: "statusIcon")
+        let icon = NSImage(named: NSImage.Name(rawValue: "statusIcon"))
         icon?.isTemplate = true
         statusItem.menu = statusMenu
         statusItem.image = icon
@@ -24,6 +24,6 @@ class StatusMenuController: NSObject {
     }
 
     @IBAction func quitClicked(_ sender: NSMenuItem) {
-        NSApplication.shared().terminate(self)
+        NSApplication.shared.terminate(self)
     }
 }
